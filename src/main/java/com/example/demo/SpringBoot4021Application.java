d14b9606e9f747330d3b0c2cbbfa92c3dc361b8e
lip2050 <k.kyartel@gmail.com>
2019-10-16 14:20:19 -0700

    model 402 assignment

---
+++ src/main/java/com/example/demo/SpringBoot4021Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot4021Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBoot4021Application.class, args);
+    }
+
+}
